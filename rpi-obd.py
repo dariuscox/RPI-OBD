9b6b37bc0fab69263ba940ca032f139b6ff80273
Daniel Cox <danielcox@daniels-mbp.wireless.rit.edu>
2017-12-02 13:46:22 -0500

    Code to print rpm on the same line if prompted. -Daniel Cox

--- rpi-obd.py
+++ rpi-obd.py
@@ -7,10 +7,24 @@ The main program for RPI - OBD
 
 import obd
 from obd import OBDStatus
+import time
 
 connection = obd.OBD()
+print("Connected")
 
-if connection.status == OBDStatus.CAR_CONNECTED:
-    print ("Success")
-else:
-    print ("you failed at life")
+r =  connection.query(obd.commands.RPM)
+
+if not r.is_null():
+    print(r.value)
+
+def new_rpm(r):
+    print("RPM: ", r.value, sep=' ', end='\r', flush=True)
+
+uin = input("What would you like to see: ")
+if(uin == "rpm"):
+    connection = obd.Async()
+    connection.watch(obd.commands.RPM, callback=new_rpm)
+    connection.start()
+# the callback will now be fired upon receipt of new values
+    time.sleep(60)
+    connection.stop()
