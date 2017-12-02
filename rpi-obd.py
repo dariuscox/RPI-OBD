00bab1f23287d469544f3089d8dd26424a7efd72
Daniel Cox <danielcox@daniels-mbp.wireless.rit.edu>
2017-12-02 15:48:11 -0500

    Data collection and display for rpm, speed, coolant temp, and fuel -Daniel Cox

--- rpi-obd.py
+++ rpi-obd.py
@@ -19,12 +19,44 @@ if not r.is_null():
 
 def new_rpm(r):
     print("RPM: ", r.value, sep=' ', end='\r', flush=True)
+def new_speed(r):
+    print("Speed: ", r.value.to("mph"), sep=' ', end='\r', flush=True)
+def new_ctemp(r):
+    print("Coolant Temperature: ", r.value, sep=' ', end='\r', flush=True)
+def new_fstat(r):
+    print("Fuel Status: ", r.value, sep=' ', end='\r', flush=True)
 
-uin = input("What would you like to see: ")
-if(uin == "rpm"):
-    connection = obd.Async()
-    connection.watch(obd.commands.RPM, callback=new_rpm)
-    connection.start()
-# the callback will now be fired upon receipt of new values
-    time.sleep(60)
-    connection.stop()
+def main():
+    while True:
+        uin = input("What would you like to see: ")
+        if(uin == "rpm"):
+            connection = obd.Async()
+            connection.watch(obd.commands.RPM, callback=new_rpm)
+            connection.start()
+        # the callback will now be fired upon receipt of new values
+            time.sleep(60)
+            connection.stop()
+        if(uin == "speed"):
+            connection = obd.Async()
+            connection.watch(obd.commands.SPEED, callback=new_speed)
+            connection.start()
+        # the callback will now be fired upon receipt of new values
+            time.sleep(60)
+            connection.stop()
+        if(uin == "ctemp"):
+            connection = obd.Async()
+            connection.watch(obd.commands.COOLANT_TEMP, callback=new_ctemp)
+            connection.start()
+        # the callback will now be fired upon receipt of new values
+            time.sleep(60)
+            connection.stop()
+        if(uin == "fstat"):
+            connection = obd.Async()
+            connection.watch(obd.commands.FUEL_STATUS, callback=new_fstat)
+            connection.start()
+        # the callback will now be fired upon receipt of new values
+            time.sleep(60)
+            connection.stop()
+        if(uin == "stop"):
+            break
+main()
